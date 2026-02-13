ce2988619a12848635fd0d30987838a47a207283
Darrell Benedict Setiawan <darrellsetiawan7@gmail.com>
2026-02-13 11:02:15 +0700

    Fix fav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
   title: "UpRev Tours - Seamless Luxury & Trusted Exploration",
   description: "Experience the world with Indonesia's most trusted travel partner since 1971.",
   icons: {
-    icon: "/UpRev Logo Mini.png",
+    icon: "UpRev.png",
   },
 };
 
